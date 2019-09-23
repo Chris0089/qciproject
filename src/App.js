b81853df8142296ea6b4057b08f50a8a59b11ff2
Christian Amparo <christian.amparo@astrazeneca.com>
2019-09-23 11:46:30 -0500

    Added materialize to project

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
+
+/*Let's create the views here and use function app as a main*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      This is my project, check this out
+    </p>
   );
 }
 
